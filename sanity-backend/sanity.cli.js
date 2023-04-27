ca1219fef2e67e5b800b99b0987cf651b3a553e4
Robert <robertmaximus383@gmail.com>
2023-04-27 22:52:33 +0300

    basic sanity setup & schemas added

---
+++ sanity-backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '4tv2cmjy',
+    dataset: 'production'
+  }
+})
---
+++ sanity-backend/schemas/index.js
@@ -0,0 +1,6 @@
+import user from './user'
+import post from './post'
+import postedBy from './postedBy'
+import comment from './comment'
+
+export const schemaTypes = [post, user, postedBy, comment]
