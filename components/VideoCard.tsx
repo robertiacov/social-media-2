a0f4b441a1622cd74a05c7714a6b1fe4715dafde
Robert <robertmaximus383@gmail.com>
2023-04-29 16:20:05 +0300

    avatar, username for users displayed

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -13,14 +13,43 @@ interface IProps {
     isShowingOnHome?: boolean;
 }
 
-const VideoCard: NextPage<IProps> = ({post}) => {
+const VideoCard: NextPage<IProps> = ({post: { caption, postedBy, video, _id, likes }, isShowingOnHome}) => {
   return (
     <div className='flex flex-col border-b-2 border-gray-200 pb-6'>
         <div>
-            <div>
-                
+            <div className='flex gap-3 p-2 cursor-pointer font-semibold rounded'>
+                <div className='md:w-16 md:h-16 w-10 h-10'>
+                    <Link href="/">
+                        <>
+                            <Image 
+                                width={62}
+                                height={62}
+                                className=' rounded-full'
+                                src={postedBy?.image}
+                                alt='user-profile'
+                                layout='responsive'
+                            />
+                        </>
+                    </Link>
+                </div>
+                <div>
+                    <Link href="/">
+                        <div className='flex items-center gap-2'>
+                            <p className='flex gap-2 items-center md:text-md font-bold text-primary'>
+                                {postedBy.userName}{' '}
+                                <GoVerified className='text-blue-400 text-md' />
+                            </p>
+                            <p className='capitalize font-medium text-xs text-gray-500 hidden md:block'>
+                                {postedBy.userName}
+                            </p>
+                            
+                        </div>
+                    </Link>
+                </div>
             </div>
         </div>
+
+        
     </div>
   )
 }
