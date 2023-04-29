72ee34bd5dc2a3e992c2c9aa449e10991e433380
Robert <robertmaximus383@gmail.com>
2023-04-29 15:04:26 +0300

    get response from getServerSideProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,4 +10,14 @@ const Home: NextPage = () => {
   )
 }
 
+export const getServerSideProps = async () => {
+  const response = await axios.get(`http://localhost:3000/api/post`)
+
+  console.log(response.data.name)
+
+  return {
+    props: {}
+  }
+}
+
 export default Home
