c73d810f6da602be4fff38d55a8a8c224a00e6ed
Robert <robertmaximus383@gmail.com>
2023-04-29 16:02:12 +0300

    basic of NoResults component

---
+++ components/NoResults.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MdOutlineVideocamOff } from 'react-icons/md';
+
+interface IProps {
+  text: string;
+}
+
+const NoResults = ({ text }: IProps) => {
+  return (
+    <div>NoResults</div>
+  )
+}
+
+export default NoResults
