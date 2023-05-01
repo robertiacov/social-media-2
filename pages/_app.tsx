b81c28989ad53f442c927a925fef49621e15d70e
Robert <robertmaximus383@gmail.com>
2023-05-01 19:30:41 +0300

    wrap app with Google Identiy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,12 @@ import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
 import { useState, useEffect } from 'react'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 import Navbar from '../components/Navbar'
 import Sidebar from '../components/Sidebar'
+
+
 
 const App = ({ Component, pageProps }: AppProps) => {
   const [isSSR, setIsSSR] = useState(true);
@@ -16,7 +19,7 @@ const App = ({ Component, pageProps }: AppProps) => {
   if (isSSR) return null;
 
   return (
-    <div>
+    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOOGLE_API_TOKEN}`}>
       <Navbar />
       <div className='flex gap-6 md:gap-20'>
         <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
@@ -26,7 +29,7 @@ const App = ({ Component, pageProps }: AppProps) => {
             <Component {...pageProps} />
         </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   )
 }
 
