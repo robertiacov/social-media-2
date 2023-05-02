22df7f9aa047ade42f73a64804979c8b9abf832c
Robert <robertmaximus383@gmail.com>
2023-05-02 19:08:12 +0300

    event functionality added for all actions

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -13,8 +13,14 @@ import { topics } from '../utils/constants';
 const Upload = () => {
     const [isLoading, setisLoading] = useState(false);
     const [videoAsset, setvideoAsset] = useState<SanityAssetDocument | undefined>();
-    const [wrongFileType, setWrongFileType] = useState(false)
+    const [wrongFileType, setWrongFileType] = useState(false);
+    const [caption, setCaption] = useState('');
+    const [category, setCategory] = useState(topics[0].name);
+    const [savingPost, setSavingPost] = useState(false);
 
+    const {userProfile} : {userProfile:any} = useAuthStore();
+
+    const router = useRouter();
 
     const uploadVideo = async (e: any) => {
         const selectedFile = e.target.files[0];
@@ -32,6 +38,34 @@ const Upload = () => {
         } else {
             setisLoading(false);
             setWrongFileType(true);
+        }
+    }
+
+    const handlePost = async () => {
+        if(caption && videoAsset?._id && category){
+            setSavingPost(true);
+
+            const document = {
+                _type: 'post',
+                caption,
+                video: {
+                    _type: 'file',
+                    asset: {
+                        _type: 'reference',
+                        _ref: videoAsset._id
+                    }
+                },
+                userId: userProfile?._id,
+                postedBy: {
+                    _type: 'postedBy',
+                    _ref: userProfile?._id
+                },
+                topic: category
+            }
+
+            await axios.post('http://localhost:3000/api/post', document);
+
+            router.push('/');
         }
     }
 
@@ -107,13 +141,13 @@ const Upload = () => {
                         </label>
                         <input 
                             type='text'
-                            value=""
-                            onChange={() => {}}
+                            value={caption}
+                            onChange={(e) => setCaption(e.target.value)}
                             className='border-2 border-gray-200 rounded p-2 outline-none text-md'    
                         />
                         <label className='text-md font-medium'>Choose a Category</label>
                         <select
-                            onChange={() => {}}
+                            onChange={(e) => setCategory(e.target.value)}
                             className='outline-none border-2 border-gray-200 text-md capitalize lg:p-4 p-2 rounded cursor-pointer'
                         >
                             {topics.map((topic, index) => (
@@ -135,7 +169,7 @@ const Upload = () => {
                                 Discard
                             </button>
                             <button
-                                onClick={() => {}}
+                                onClick={handlePost}
                                 type='button'
                                 className='bg-[#F51997] text-white text-md font-medium p-2 rounded w-28 lg:w-44 outline-none'
                             >
