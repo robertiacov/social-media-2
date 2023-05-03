83e9900f39670310a961767e7169bfe24a0b7044
Robert <robertmaximus383@gmail.com>
2023-05-03 11:53:04 +0300

    video page updated to show a video

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -17,11 +17,65 @@ interface IProps {
 
 const Detail = ({postDetails}: IProps) => {
     const [post, setPost] = useState(postDetails);
+    const [playing, setPlaying] = useState(false);
+    const [isVideoMuted, setIsVideoMuted] = useState(false)
+    const videoRef = useRef<HTMLVideoElement>(null);
 
-    const videoRef = useRef(null);
+    const onVideoClick = () => {
+        if (playing) {
+            videoRef?.current?.pause();
+            setPlaying(false);
+          } else {
+            videoRef?.current?.play();
+            setPlaying(true);
+          }
+    }
+
+
+    if(!post) return null;
 
   return (
-    <div>Detail</div>
+    <div className='flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap'>
+        <div className='relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center'>
+            <div className='opacity-90 absolute top-6 left-2 lg:left-6 flex gap-6 z-50'>
+                <p className='cursor-pointer '>
+                    <MdOutlineCancel className='text-white text-[35px] hover:opacity-90' />
+                </p>
+            </div>
+            <div className='relative'>
+                <div className='lg:h-[100vh] h-[60vh]'>
+                    <video
+                        ref={videoRef}
+                        loop
+                        onClick={onVideoClick}
+                        src={post.video.asset.url}
+                        className='h-full cursor-pointer'
+                    >
+                    </video>
+                </div>
+                <div className='absolute top-[45%] left-[40%]  cursor-pointer'>
+                {!playing && (
+                  <button onClick={onVideoClick}>
+                    <BsFillPlayFill className='text-white text-6xl lg:text-8xl' />
+                  </button>
+                )}
+              </div>
+            </div>
+
+            <div className='absolute bottom-5 lg:bottom-10 right-5 lg:right-10  cursor-pointer'>
+                {isVideoMuted ? (
+                    <button onClick={() => setIsVideoMuted(false)}>
+                        <HiVolumeOff className='text-white text-2xl lg:text-4xl' />
+                    </button>
+                ) : (
+                    <button onClick={() => setIsVideoMuted(true)}>
+                        <HiVolumeUp className='text-white text-2xl lg:text-4xl' />
+                    </button>
+                )}
+            </div>
+
+        </div>
+    </div>
   )
 }
 
