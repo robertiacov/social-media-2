2e333abf1c0e8400e4847fb786613ebbdbf70a16
Robert <robertmaximus383@gmail.com>
2023-05-03 13:05:37 +0300

    fixed duplicate like from same user

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -13,14 +13,19 @@ interface IProps{
 
 const LikeButton = ({likes, handleLike, handleDislike}:IProps) => {
     const [alreadyLiked, setAlreadyLiked] = useState(false)
-    const {userProfile} = useAuthStore();
+    const {userProfile}: any = useAuthStore();
+    const filterLikes = likes?.filter((item) => item._ref === userProfile?._id)
 
     useEffect(() => {
-
-    }, [likes])
+        if(filterLikes?.length > 0){
+            setAlreadyLiked(true);
+        } else {
+            setAlreadyLiked(false)
+        }
+    }, [filterLikes, likes])
 
   return (
-    <div className={`gap-6`}>
+    <div className={`flex gap-6`}>
         <div className='mt-4 flex flex-col justify-center items-center cursor-pointer'>
             {alreadyLiked ? (
                 <div className='bg-primary rounded-full p-2 md:p-4 text-[#F51997] ' onClick={handleDislike} >
