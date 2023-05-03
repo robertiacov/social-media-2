4f3988c6635cd3ff47708507b1f3a6e98ba71844
Robert <robertmaximus383@gmail.com>
2023-05-03 11:27:16 +0300

    pass data from detail page req and the props for video type

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -9,8 +9,17 @@ import { HiVolumeUp, HiVolumeOff } from 'react-icons/hi';
 
 import axios from 'axios';
 import { BASE_URL } from '../../utils';
+import { Video } from '../../types';
 
-const Detail = () => {
+interface IProps {
+    postDetails: Video,
+}
+
+const Detail = ({postDetails}: IProps) => {
+    const [post, setPost] = useState(postDetails);
+
+    const videoRef = useRef(null);
+
   return (
     <div>Detail</div>
   )
@@ -18,8 +27,12 @@ const Detail = () => {
 
 export const getServerSideProps = async ({
     params: {id}
-}) => {
+} : { params: {id: string}}) => {
     const {data} = await axios.get(`${BASE_URL}/api/post/${id}`)
+
+    return {
+        props: {postDetails: data}
+    }
 }
 
 export default Detail
