5cafc32a89e7abae86bf9a227856a14e6861a8fe
Robert <robertmaximus383@gmail.com>
2023-05-04 11:41:46 +0300

    comments component basics implemented

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { Dispatch, SetStateAction } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { GoVerified } from 'react-icons/go';
+
+import useAuthStore from '../store/authStore';
+import NoResults from './NoResults';
 
 const Comments = () => {
+const comments = [];
+
   return (
-    <div>Comments</div>
+    <div className='border-t-2 border-gray-200 pt-4 px-10 mt-4 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]'>
+      <div className='overflow-scroll lg:h-[457px]'>
+        {comments.length ? (
+          <div>videos</div>
+        ) : (
+          <NoResults text='No Comments Yet! Be First to do add the comment.'/>
+        )}
+      </div>
+    </div>
   )
 }
 
