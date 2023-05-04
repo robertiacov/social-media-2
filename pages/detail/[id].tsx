a1725352c9d615ad943af75718c5991b0da43a93
Robert <robertmaximus383@gmail.com>
2023-05-04 12:44:34 +0300

    add comment functionality added

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -26,6 +26,8 @@ const Detail = ({postDetails}: IProps) => {
     const videoRef = useRef<HTMLVideoElement>(null);
     const router = useRouter();
     const {userProfile}: any = userAuthStore();
+    const [comment, setComment] = useState("")
+    const [isPostingComment, setIsPostingComment] = useState(false)
 
     const onVideoClick = () => {
         if (playing) {
@@ -52,6 +54,23 @@ const Detail = ({postDetails}: IProps) => {
             })
 
             setPost({ ...post, likes: data.likes});
+        }
+    }
+
+    const addComment = async (e: { preventDefault: () => void }) => {
+        e.preventDefault();
+
+        if (userProfile && comment) {
+            setIsPostingComment(true)
+
+            const {data} = await axios.put(`${BASE_URL}/api/post/${post._id}`, {
+                userId: userProfile._id,
+                comment
+            });
+
+            setPost({ ...post, comments: data.comments });
+            setComment('');
+            setIsPostingComment(false);
         }
     }
 
@@ -132,7 +151,11 @@ const Detail = ({postDetails}: IProps) => {
                 </div>
             </div>
             <Comments 
-            
+                comment = {comment}
+                setComment = {setComment}
+                addComment = {addComment}
+                comments = {post.comments}
+                isPostingComment = {isPostingComment}
             />
 
         </div>
