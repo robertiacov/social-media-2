970f2183be90f6bd0677af83a7aad0c400cb193e
Robert <robertmaximus383@gmail.com>
2023-05-04 22:46:49 +0300

    comments fetched and displayed on details page

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -5,6 +5,7 @@ import { GoVerified } from 'react-icons/go';
 
 import useAuthStore from '../store/authStore';
 import NoResults from './NoResults';
+import { IUser } from '../types';
 
 interface IProps {
   isPostingComment: Boolean;
@@ -23,13 +24,49 @@ interface IComment {
 
 const Comments = ({ comment, setComment, addComment, comments, isPostingComment }: IProps) => {
 
-  const {userProfile} = useAuthStore();
+  const {userProfile, allUsers} = useAuthStore();
 
   return (
     <div className='border-t-2 border-gray-200 pt-4 px-10 mt-4 bg-[#F8F8F8] border-b-2 lg:pb-0 pb-[100px]'>
       <div className='overflow-scroll lg:h-[457px]'>
         {comments?.length > 0 ? (
-          <div>videos</div>
+          comments.map((item, idx) => (
+            <>
+              {allUsers.map((user: IUser) => (
+                user._id === (item.postedBy._id || item.postedBy._ref) && (
+                  <div className=' p-2 items-center' key={idx}>
+                    <Link href={`/profile/${user._id}`}>
+                      <div className='flex items-start gap-3'>
+                        <div className='w-12 h-12'>
+                            <Image
+                              width={48}
+                              height={48}
+                              className='rounded-full cursor-pointer'
+                              src={user.image}
+                              alt='user-profile'
+                              layout='responsive'
+                            />
+                        </div>
+
+                        <div className='hidden xl:block'>
+                          <p className='flex gap-1 items-center text-md font-bold text-primary lowercase cursor-pointer'>
+                            {user.userName.replace(/\s+/g, '')}{' '}
+                            <GoVerified className='text-blue-400' />
+                          </p>
+                          
+                        </div>
+                      </div>
+                    </Link>
+                    <div>
+                        <p className='-mt-6 ml-16 text-[16px] mr-8'>
+                          {item.comment}
+                        </p>
+                    </div>
+                  </div>
+                )
+              ))}
+            </>
+          ))
         ) : (
           <NoResults text='No Comments Yet!'/>
         )}
