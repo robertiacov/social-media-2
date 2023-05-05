22777c4fc85954230fa26f8e1735315c2d4333e5
Robert <robertmaximus383@gmail.com>
2023-05-05 13:51:38 +0300

    search form (style) added to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,6 +7,7 @@ import { AiOutlineLogout} from 'react-icons/ai'
 import { BiSearch } from 'react-icons/bi'
 import { IoMdAdd } from 'react-icons/io'
 
+import { IUser } from '../types';
 import Logo from '../utils/tiktik-logo.png'
 import { createOrGetUser } from '../utils'
 
@@ -14,6 +15,10 @@ import userAuthStore from '../store/authStore'
 
 const Navbar = () => {
   const {userProfile, addUser, removeUser} = userAuthStore();
+
+  const handleSearch = () => {
+
+  }
   
   return (
     <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
@@ -28,7 +33,25 @@ const Navbar = () => {
             </div>
         </Link>
 
-        <div>SEARCH</div>
+        <div className='relative hidden md:block'>
+          <form
+            className='absolute md:static top-10 -left-20 bg-white'
+            onSubmit={handleSearch}
+          >
+            <input 
+              type="text"
+              onChange={() => {}}
+              placeholder='Search accounts and videos'
+              className='bg-primary p-3 md:text-md font-medium border-2 border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] md:w-[350px] rounded-full  md:top-0'
+            />
+            <button
+              onClick={handleSearch}
+              className='absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400'
+            >
+              <BiSearch />
+            </button>
+          </form>
+        </div>
 
         <div>
           {userProfile ? (
