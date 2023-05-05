586f65454ee857942166afec96a5b4e2a16a7338
Robert <robertmaximus383@gmail.com>
2023-05-05 18:33:22 +0300

    basics of search page implemented

--- pages/search/[searchTerm].tsx
+++ pages/search/[searchTerm].tsx
@@ -11,9 +11,23 @@ import useAuthStore from '../../store/authStore';
 import { BASE_URL } from '../../utils';
 import { IUser, Video } from '../../types';
 
-const Search = () => {
+const Search = ({ videos }: { videos: Video[] }) => {
+    const [isAccounts, setIsAccounts] = useState(false);
+
+    const accounts = isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
+    const isVideos = !isAccounts ? 'border-b-2 border-black' : 'text-gray-400';
+
   return (
-    <div>Search</div>
+    <div className='w-full'>
+        <div className='flex gap-10 mb-10 border-b-2 border-gray-200 md:fixed z-50 bg-white w-full'>
+            <p onClick={() => setIsAccounts(true)} className={`text-xl  font-semibold cursor-pointer ${accounts} mt-2`}>
+                Accounts
+            </p>
+            <p className={`text-xl font-semibold cursor-pointer ${isVideos} mt-2`} onClick={() => setIsAccounts(false)}>
+                Videos
+            </p>
+        </div>
+    </div>
   )
 }
 
