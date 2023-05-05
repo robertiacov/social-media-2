ca5feb7e0be28e421bfa49202d11dd650580dd5c
Robert <robertmaximus383@gmail.com>
2023-05-05 14:04:26 +0300

    basic search page created w/o endpoint for server side props

---
+++ pages/search/[searchTerm].tsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { GoVerified } from 'react-icons/go';
+import Link from 'next/link';
+import axios from 'axios';
+
+import NoResults from '../../components/NoResults';
+import VideoCard from '../../components/VideoCard';
+import useAuthStore from '../../store/authStore';
+import { BASE_URL } from '../../utils';
+import { IUser, Video } from '../../types';
+
+const Search = () => {
+  return (
+    <div>Search</div>
+  )
+}
+
+export const getServerSideProps = async ({
+    params: { searchTerm },
+  }: {
+    params: { searchTerm: string };
+  }) => {
+    const res = await axios.get(`${BASE_URL}/api/search/${searchTerm}`);
+  
+    return {
+      props: { videos: res.data },
+    };
+  };
+
+export default Search
