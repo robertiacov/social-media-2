97fb2c6333d181fb7991dc9e9c1ac168a551511f
Robert <robertmaximus383@gmail.com>
2023-05-05 13:04:46 +0300

    server side props to fetch data from api

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Image from 'next/image';
+import { GoVerified } from 'react-icons/go';
+import axios from 'axios';
 
-const Profile = () => {
+import VideoCard from '../../components/VideoCard';
+import NoResults from '../../components/NoResults';
+import { IUser, Video } from '../../types';
+import { BASE_URL } from '../../utils';
+
+interface IProps {
+    data: {
+      user: IUser;
+      userVideos: Video[];
+      userLikedVideos: Video[];
+    };
+  }
+
+const Profile = ({data}: IProps) => {
   return (
     <div>Profile</div>
   )
+}
+
+export const getServerSideProps = async ({
+    params: { id },
+} : {
+    params: { id: string }
+}) => {
+    const res = await axios.get(`${BASE_URL}/profile/${id}`)
+
+    return {
+        props: { data: res.data }
+    }
 }
 
 export default Profile
