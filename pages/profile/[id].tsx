33a4ee681a7e29355f2490d512e6827609e80e3f
Robert <robertmaximus383@gmail.com>
2023-05-05 13:28:44 +0300

    user profile image and name && videos / liked videos tabs added

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -17,8 +17,54 @@ interface IProps {
   }
 
 const Profile = ({data}: IProps) => {
+
+    const { user, userVideos, userLikedVideos } = data;
+    const [showUserVideos, setShowUserVideos] = useState(true);
+
+    const videos = showUserVideos ? 'border-b-2 border-black' : 'text-gray-400';
+    const liked = !showUserVideos ? 'border-b-2 border-black' : 'text-gray-400';
+
+
   return (
-    <div>Profile</div>
+    <div className='w-full'>
+        <div className='flex gap-6 md:gap-10 mb-4 bg-white w-full'>
+            <div className='w-16 h-16 md:w-32 md:h-32'>
+                <Image
+                    width={120}
+                    height={120}
+                    layout='responsive'
+                    className='rounded-full'
+                    src={user.image}
+                    alt='user-profile'
+                />
+            </div>
+
+            <div>
+                <div className='text-md md:text-2xl font-bold tracking-wider flex gap-1 items-center justify-center lowercase'>
+                    <span>{user.userName.replace(/\s+/g, '')} </span>
+                    <GoVerified className='text-blue-400 md:text-xl text-md' />
+                </div>
+                <p className='capitalize md:text-xl text-gray-400 text-xs'> {user.userName}</p>
+            </div>
+        </div>
+
+        <div>
+            <div className='flex gap-10 mb-10 mt-10 border-b-2 border-gray-200 bg-white w-full'>
+                <p className={`text-xl font-semibold cursor-pointer mt-2 ${videos}`} onClick={() => setShowUserVideos(true)}>
+                    Videos
+                </p>
+                <p className={`text-xl font-semibold cursor-pointer mt-2 ${liked}`} onClick={() => setShowUserVideos(false)}>
+                    Liked
+                </p>
+            </div>
+
+            <div className='flex gap-6 flex-wrap md:justify-start'>
+
+            </div>
+        </div>
+        
+
+    </div>
   )
 }
 
