50694a54b644f8074cde2a3f64c8e8c327dd863d
Robert <robertmaximus383@gmail.com>
2023-05-05 14:17:48 +0300

    endpoint for search on server created

---
+++ pages/api/search/[searchTerm].ts
@@ -0,0 +1,17 @@
+import { client } from '../../../utils/client';
+import { searchPostsQuery } from '../../../utils/queries';
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+
+export default async function handler(req: NextApiRequest,
+res: NextApiResponse) {
+  if(req.method === 'GET') {
+    const {searchTerm} = req.query;
+
+    const videosQuery = searchPostsQuery(searchTerm);
+
+    const videos = await client.fetch(videosQuery);
+
+    res.status(200).json(videos);
+  }
+}
