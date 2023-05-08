3b8a5bb0a1d82e0a9cbfe69fe2b78cd832828d06
Robert <robertmaximus383@gmail.com>
2023-05-08 12:56:22 +0300

    background image link update & design update on video card and details page

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -82,7 +82,7 @@ const VideoCard: NextPage<IProps> = ({post: { caption, postedBy, video, _id, lik
                         loop
                         ref={videoRef}
                         src={video.asset.url}
-                        className='lg:w-[600px] h-[300px] md:h-[400px] lg:h-[528px] w-[200px] rounded-2xl cursor-pointer bg-gray-100'
+                        className='lg:w-[600px] h-[300px] md:h-[400px] lg:h-[528px] w-[200px] rounded-2xl cursor-pointer bg-blurred-img bg-no-repeat bg-cover bg-center'
                     >
 
                     </video>
--- tailwind.config.js
+++ tailwind.config.js
@@ -35,7 +35,7 @@ module.exports = {
       },
       backgroundImage: {
         'blurred-img':
-          "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSsaaJ7s4lqcBF4IDROVPzrlL5fexcwRmDlnuEYQenWTt1DejFY5kmYDref2a0Hp2eE4aw&usqp=CAU')",
+          "url('https://drive.google.com/uc?export=view&id=1ttBRPlD4aGF2Y0yElTuSrA_xhO-VqOZk')",
       },
     },
   },
