085586950bf6f7254a7cb70b641d5d1dd35c1d8d
Robert <robertmaximus383@gmail.com>
2023-05-08 12:57:56 +0300

    footer updated

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = () => {
       <List items={footerList1} mt={false} />
       <List items={footerList2} mt />
       <List items={footerList3} mt />
-      <p className='text-gray-400 text-sm mt-5'>© 2023 TikTik</p>
+      <p className='text-gray-400 text-sm mt-5'>© 2023 Vid Vers</p>
     </div>
   )
 }
